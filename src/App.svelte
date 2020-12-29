5191c4affe8f3bd58a0f5b5fbb101406bbe04736
nova <courtois1337@gmail.com>
2020-12-28 20:48:31 -0800

    create top row

--- src/App.svelte
+++ src/App.svelte
@@ -15,12 +15,88 @@
 	<KeyboardRow cssClass={'keyboard-row--util'}>
 		<KeyboardRowInner cssClass={'key'}>
 			<KeyOutline location={''}>
-				<Key cssClass={''}>
-					<KeyText text={'hi'}></KeyText>
+				<Key cssClass={'key--util key--esc'}>
+					<KeyText text={'esc'}></KeyText>
 				</Key>
-
 			</KeyOutline>
 
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f1'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f2'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f3'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f4'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f5'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f6'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f7'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f8'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f9'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f10'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f11'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f'}>
+					<KeyText text={'f12'}></KeyText>
+				</Key>
+			</KeyOutline>
+
+			<KeyOutline location={''}>
+				<Key cssClass={'key--util key--f key--power'}>
+					<KeyText text={'&#x23FB;'}></KeyText>
+				</Key>
+			</KeyOutline>
 		</KeyboardRowInner>
 	</KeyboardRow>
 
